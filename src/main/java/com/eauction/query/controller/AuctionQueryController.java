cc3dcaaee0bf3408560593ab7ca1d51df0a35ace
shylesh <shyleshgiri.tv@cognizant.com>
2022-07-03 11:57:32 +0530

    Enabled cross origin

--- src/main/java/com/eauction/query/controller/AuctionQueryController.java
+++ src/main/java/com/eauction/query/controller/AuctionQueryController.java
@@ -9,6 +9,7 @@ import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import com.eauction.query.dto.PlacedBid;
 import com.eauction.query.exception.UserNotFoundException;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/e-auction/api/v1/query")
 public class AuctionQueryController {
 	
